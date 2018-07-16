7ff72358e2c979349f2c465c7c00089f0c17b1a0
HaTT2018 <zkaihang17@gmail.com>
2018-07-16 21:29:17 +0800

    first commit

---
+++ source/LightControl/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace LightControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.button1.Location = new System.Drawing.Point(13, 502);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 47);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(266, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(616, 536);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(233, 469);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "导入图片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(890, 13);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(238, 536);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 562);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "LightControl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+
+        //public EventHandler pictureBox1_Click { get; private set; }
+    }
+}
+
---
+++ source/LightControl/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CognitiveServices.Speech;
+using Newtonsoft.Json;
+
+namespace LightControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "开始识别";
+            //pictureBox1.Load("LightOff.png");
+        }
+
+        // 语音识别器
+        SpeechRecognizer recognizer;
+        bool isRecording = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // 第一步
+                // 初始化语音服务SDK并启动识别器，进行语音转文本
+                // 密钥和区域可在 https://azure.microsoft.com/zh-cn/try/cognitive-services/my-apis/?api=speech-services 中找到
+                // 密钥示例: 5ee7ba6869f44321a40751967accf7a9
+                // 区域示例: westus
+                SpeechFactory speechFactory = SpeechFactory.FromSubscription("2caea042ef27427189fd56be10425585", "westus");
+
+                // 识别中文
+                recognizer = speechFactory.CreateSpeechRecognizer("en-US");
+                //等待识别中
+                recognizer.IntermediateResultReceived += Recognizer_IntermediateResultReceived;
+                // 识别的最终结果
+                recognizer.FinalResultReceived += Recognizer_FinalResultReceived;
+                // 出错时的处理
+                recognizer.RecognitionErrorRaised += Recognizer_RecognitionErrorRaised;
+            }
+            catch (Exception ex)
+            {
+                if (ex is System.TypeInitializationException)
+                {
+                    Log("语音SDK不支持Any CPU, 请更改为x64");
+                }
+                else
+                {
+                    Log("初始化出错，请确认麦克风工作正常");
+                    Log("已降级到文本语言理解模式");
+
+                    TextBox inputBox = new TextBox();
+                    inputBox.Text = "";
+                    inputBox.Size = new Size(300, 26);
+                    inputBox.Location = new Point(10, 10);
+                    inputBox.KeyDown += inputBox_KeyDown;
+                    Controls.Add(inputBox);
+
+                    button1.Visible = false;
+                }
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+            //以下代码已经实现了识别语音内容（中文），现在应该把语音内容与照片数据集中的caption注解进行匹配，
+            //将结果显示在picturebox中，同时在最右边的textbox2中按照匹配率排序，显示各个图片的图片名（**.jpg|png|...）
+            //最后能实现点一个名字，显示一张图片的功能（我不确定是不是应该用textbox，或许是用listbox或者checkbox…）
+        {
+            button1.Enabled = false;
+
+            isRecording = !isRecording;
+            if (isRecording)
+            {
+                // 启动识别器
+                await recognizer.StartContinuousRecognitionAsync();
+                button1.Text = "停止识别";
+            }
+            else
+            {
+                // 停止识别器
+                await recognizer.StopContinuousRecognitionAsync();
+                button1.Text = "开始识别";
+            }
+            
+            button1.Enabled = true;
+        }
+
+        //等待识别中
+        private void Recognizer_IntermediateResultReceived(object sender, SpeechRecognitionResultEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(e.Result.Text))
+                {
+                Log("recognizing...");               
+            }
+        }
+        // 识别的最终结果
+        private void Recognizer_FinalResultReceived(object sender, SpeechRecognitionResultEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Result.Text))
+            {
+                Log("最终结果: " + e.Result.Text);
+            }
+        }
+
+        // 出错时的处理
+        private void Recognizer_RecognitionErrorRaised(object sender, RecognitionErrorEventArgs e)
+        {
+            Log("错误: " + e.FailureReason);
+        }
+
+
+        #region 界面操作
+
+        private void Log(string message)
+        {
+            MakesureRunInUI(() =>
+            {
+                textBox1.AppendText(message + "\r\n");
+            });
+        }
+
+
+        private void MakesureRunInUI(Action action)
+        {
+            if (InvokeRequired)
+            {
+                MethodInvoker method = new MethodInvoker(action);
+                Invoke(action, null);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        #endregion
+
+        private void inputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && sender is TextBox)
+            {
+                TextBox textBox = sender as TextBox;
+                e.Handled = true;
+                Log(textBox.Text);
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //导入图片按钮应该链接一个装有很多图片的文件夹，并在最右边的textbox中显示这些文件的名称
+            //然后同时再调用图像识别的代码，将图片与其caption一一对应
+            //最后生成一个数据集（库），保存在本地或者内存中（不知道这么说对不对）
+        }
+    }
+}
